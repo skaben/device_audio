28ea87c93e860345479930395b150cc69614f5c0
Dmitry Bis <zerthmonk@pm.me>
2020-06-04 13:19:32 +0000

    remove unnecessary comments, minimal codestyle

--- device.py
+++ device.py
@@ -11,6 +11,11 @@ from skabenclient.helpers import make_event
 from config import SoundConfig
 
 
+class RaiseFlag(Exception):
+    """Base class for raising flag."""
+    pass
+
+
 class SoundDevice(BaseDevice):
 
     """ Test device should be able to generate all kind of messages
@@ -24,29 +29,17 @@ class SoundDevice(BaseDevice):
     def __init__(self, system_config, device_config, **kwargs):
         super().__init__(system_config, device_config)
         self.running = None
-        # создаем директорию сначала, вообще все константы вначале объявляем
-        # я бы также попросил ее создавать вот тут, это стандарт для терминала и замка
         self.sound_path = os.path.join(system_config.root, 'resources', 'sound')
         self.phrase = self.config.get('phrase')
         self.snd_files = self.config.get('sound_files')
         try:
-            """ 
-               логирование доступно через self.logger
-               советую почитать таки BaseDevice - станет чуть яснее, как с этим жить)
-               по поводу self.logger.exception два замечания:
-               
-               2. self.logger.exception имеет смысл использовать после except, а тут его нет.
-               пунктом 1 я возможно сам где-то грешу - если это так, то это неправильно.
-            """
             if not os.path.exists(self.sound_path):
                 self.logger.warning(f'Path for sound files {self.sound_path} not found. Creating...\n')
                 os.makedirs(self.sound_path)
         except TypeError as exception_text:
-            # а вот тут вполне себе сработает и трейсбек еще перехватит
             exc = f'cannot create directory for sound files\n{exception_text}'
             self.logger.exception(exc)
             raise Exception(exc)
-        # потом уже логика. если нет созданной директории - лоадер плюнет исключением, т.к. неоткуда лоадить.
         self.snd = self._snd_init(self.sound_path)
         self.snd_check()
         self.snd_base_get()
@@ -64,28 +57,26 @@ class SoundDevice(BaseDevice):
             change_flag = False
             play_flag = self.config.get('play')
             if play_flag:
-                if(phrase['repeat']>0):
-                    range_iter = range(0,phrase['repeat'])
+                if phrase['repeat'] > 0:
+                    range_iter = range(0, phrase['repeat'])
                 else:
-                    range_iter = cycle(range(0,len(phrase['content'])))
-                for i in range_iter:
-                    for snd_name in phrase['content']:
-                        for j in range(0,snd_name['repeat']):
-                            snd_p = (self.snd_files[snd_name['name']]['file'].split('.'))[0]
-                            self.snd.play(sound=snd_p, channel='fg')
-                            while self.snd.channels['fg'].get_busy():
-                                phrase_new = self.config.get('phrase')
-                                play_flag = self.config.get('play')
-                                if (phrase_new != phrase or not play_flag):
-                                    change_flag = True
-                                    break
-                            if (change_flag):
-                                break
-                        if (change_flag):
-                            break
-                    if (change_flag):
-                        break
-                if (change_flag):
+                    range_iter = cycle(range(0, len(phrase['content'])))
+                try:
+                    for i in range_iter:
+                        for snd_name in phrase['content']:
+                            for j in range(0, snd_name['repeat']):
+                                snd_p = (self.snd_files[snd_name['name']]['file'].split('.'))[0]
+                                self.snd.play(sound=snd_p, channel='fg')
+                                while self.snd.channels['fg'].get_busy():
+                                    phrase_new = self.config.get('phrase')
+                                    play_flag = self.config.get('play')
+                                    if (phrase_new != phrase or not play_flag):
+                                        change_flag = True
+                                        raise RaiseFlag('change flag')
+                except RaiseFlag:
+                    pass
+                
+                if change_flag:
                     phrase = phrase_new
                 else:
                     play_flag = False
@@ -96,15 +87,7 @@ class SoundDevice(BaseDevice):
         for key, val in self.snd_files.items():
             snd_full_name = os.path.join(self.sound_path, val['file'])
             val['loaded'] = os.path.isfile(snd_full_name)
-
-    def snd_get(self, snd_url, snd_file):
-        try:
-            urllib.request.urlretrieve(snd_url, os.path.join(self.sound_path, snd_file))
-        except Exception as e:
-            self.logger.error(f'cannot retrieve {snd_url}:\n{e}')
-            return (False)
-        return (True)
-
+            
     def _snd_init(self, sound_dir):
         try:
             snd = SoundLoader(sound_dir=sound_dir)
@@ -112,13 +95,22 @@ class SoundDevice(BaseDevice):
             self.logger.exception(f'failed to initialize sound module:\n{e}')
             snd = None
         return snd
+        
+    def snd_get(self, snd_url, snd_file):
+        try:
+            target = os.path.join(self.sound_path, snd_file)
+            urllib.request.urlretrieve(snd_url, target)
+            return True
+        except Exception as e:
+            self.logger.error(f'cannot retrieve {snd_url}:\n{e}')
 
     def snd_base_get(self):
         snd_files_tmp = self.snd_files.copy()
         for key, value in snd_files_tmp.items():
             loaded = value.get('loaded')
             if not loaded:
-                self.snd_files[key]['loaded'] = self.snd_get(value.get('remote'), value.get('file'))
+                self.snd_files[key]['loaded'] = self.snd_get(value.get('remote'), 
+                                                             value.get('file'))
         self.state_update({'sound_files':self.snd_files})
 
 
